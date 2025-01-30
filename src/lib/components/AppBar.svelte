0482a939105a0030ee8c00fcf01adddec79c6e6d
Tim Kluge <ubrfu@student.kit.edu>
2025-01-30 01:09:39 +0100

    style: add page layout

---
+++ src/lib/components/AppBar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import TopAppBar, {
+    Row,
+    Section,
+    Title,
+    AutoAdjust,
+  } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+
+  let { smuiBar = $bindable() } = $props();
+</script>
+
+<TopAppBar
+  bind:this={smuiBar}
+  variant="fixed">
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Fixed</Title>
+    </Section>
+    <Section>
+      <div class="flex justify-center items-center size-full">
+        <Title class="p-0">Title</Title>
+      </div>
+    </Section>
+    <Section
+      align="end"
+      toolbar>
+      <IconButton
+        class="material-icons"
+        aria-label="Download">file_download</IconButton>
+      <IconButton
+        class="material-icons"
+        aria-label="Print this page">print</IconButton>
+      <IconButton
+        class="material-icons"
+        aria-label="Bookmark this page">bookmark</IconButton>
+    </Section>
+  </Row>
+</TopAppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
   import { invalidate } from '$app/navigation';
   import { onMount } from 'svelte';
+  import '../app.css';
+  import TopAppBar, { AutoAdjust } from '@smui/top-app-bar';
+  import AppBar from '$lib/components/AppBar.svelte';
 
   let { data, children } = $props();
   let { session, supabase } = $derived(data);
+
+  let topAppBar: TopAppBar | null = $state(null);
 
   onMount(() => {
     const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
@@ -16,4 +21,18 @@
   });
 </script>
 
-{@render children()}
+<!-- divide page into 3 rows: header, main content, and footer -->
+<div class="min-h-screen grid grid-rows-[auto_1fr_auto]">
+  <!-- header -->
+  <header>
+    <AppBar bind:smuiBar={topAppBar} />
+  </header>
+  <!-- main content -->
+  <AutoAdjust {topAppBar}>
+    <main class="size-full">
+      {@render children()}
+    </main>
+  </AutoAdjust>
+  <!-- footer -->
+  <footer>(footer)</footer>
+</div>
