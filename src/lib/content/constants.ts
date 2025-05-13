b0dac53020ad7d4f380d728e7a336721ed0a9c47
Tim Kluge <ubrfu@student.kit.edu>
2025-05-13 18:53:36 +0200

    feat: change payment method to incorporate stripe

--- src/lib/content/constants.ts
+++ src/lib/content/constants.ts
@@ -3,11 +3,6 @@ export const DEFAULT_AVATAR_URL =
 export const DEFAULT_SONG_COVER_URL =
   'https://przfigmcivbydgrusirj.supabase.co/storage/v1/object/public/images//cover-fallback.png';
 export const PARTICIPANT_LIMIT = 250;
-export const BANK_ACCOUNT_DETAILS = {
-  ACCOUNT_HOLDER: 'Chad e.V.',
-  IBAN: 'DE12345678901234567890',
-  BIC: 'DEUTDEDBFRA',
-};
 export const CONTACT = {
   EMAIL: 'orga@chad25.com',
   INSTAGRAM: 'https://www.instagram.com/chadtagung',
---
+++ src/lib/server/constants.ts
@@ -0,0 +1,5 @@
+export const CHECKOUT_DATA = {
+  defaultLink: 'https://buy.stripe.com/8wM5llc1degG144cMP',
+  discountLink: 'https://buy.stripe.com/9AQeVVd5hb4udQQeUY',
+  cutoff: 80,
+};
