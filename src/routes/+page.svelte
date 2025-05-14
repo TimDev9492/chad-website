64f2bf086bf0ed11440802923fcbf6942d3bb99e
Tim Kluge <ubrfu@student.kit.edu>
2025-05-14 22:27:32 +0200

    fix: fix video not autoplaying on mobile safari

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -84,6 +84,7 @@
         autoplay={true}
         muted={true}
         loop={true}
+        playsinline={true}
       >
       </video>
       <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
