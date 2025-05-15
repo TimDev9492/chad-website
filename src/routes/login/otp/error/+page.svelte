78e10fb03947fded81bb4c4fe2afb1f8574584f2
Tim Kluge <ubrfu@student.kit.edu>
2025-05-15 21:51:54 +0200

    fix: fix layout for mobile on /login/otp/error

--- src/routes/login/otp/error/+page.svelte
+++ src/routes/login/otp/error/+page.svelte
@@ -1,4 +1,6 @@
 <div class="size-full flex flex-col justify-center items-center">
-  <div class="mdc-typography--headline4 mb-8">Anmeldung fehlgeschlagen!</div>
+  <div class="mdc-typography--headline4 mb-8 text-center">
+    Anmeldung fehlgeschlagen!
+  </div>
   <a href="/">Zurück zum Start</a>
 </div>
